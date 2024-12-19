508e57eb2976d0590e6697d2be968505d09735e3
Mario Alberto Nonato Carrillo <mnonatoc@toluca.tecnm.mx>
2024-12-19 16:30:33 -0600

    feat(/): Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import {Montserrat, Lusitana, Inter} from 'next/font/google'
+
+export const montserrat = Montserrat({
+    subsets :['latin'],
+    weight: '400'
+})
+
+export const lusitana = Lusitana({
+    subsets : ['latin'],
+    weight: '400'
+})
+
+export const inter = Inter({
+    subsets : ['latin']
+})
